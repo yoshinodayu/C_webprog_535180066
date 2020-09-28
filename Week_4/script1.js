1bd8dcc9ba21aa03799c356e8ddcd66ef941f45c
yoshinodayu <70676229+yoshinodayu@users.noreply.github.com>
2020-09-28 13:43:53 +0700

    .

---
+++ Week_4/script1.js
@@ -0,0 +1,22 @@
+var x = 5;
+var y = 15;
+var z = x + y;
+document.write('<h1>' + "The value of Z is: " + z + '</h1>'); 
+console.log(z);
+console.log(typeof "-123.30");
+
+var pulpen;
+pulpen = ["red", "blue", "green", "orange"];
+
+var pulpenMerah = pulpen[0];
+console.log(pulpenMerah);
+
+var result;
+
+function myFunction(p1, p2){
+    var hasil = p1 * p2;
+    return hasil;
+}
+
+result = (myFunction(4, 3));
+document.write(result);
