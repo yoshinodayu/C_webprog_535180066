61ebaea7554128b6fd31c5d6d4505dc43db595af
yoshinodayu <70676229+yoshinodayu@users.noreply.github.com>
2020-10-01 10:12:21 +0700

    .

---
+++ Week_4 JavaScript/script.js
@@ -0,0 +1,13 @@
+function changeLine1Style() {
+
+    document.getElementById("cLine1").className = "MakeRed";
+
+}
+
+
+
+function ChangeStyle(id, classname){
+
+    document.getElementById(id).className = classname;
+
+}
