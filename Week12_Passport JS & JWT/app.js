106b664be68c1c2ddca2146c2b40ee272355c13a
yoshinodayu <70676229+yoshinodayu@users.noreply.github.com>
2020-12-10 10:41:53 +0700

    .

--- Week12_Passport JS & JWT/app.js
+++ Week12_Passport JS & JWT/app.js
@@ -59,7 +59,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/", require("./routes/index"));
-app.use("/users", require("./routes/users"));
+app.use("/users", require("./routes/user"));
 
 const port = process.env.PORT || 3000;
 
